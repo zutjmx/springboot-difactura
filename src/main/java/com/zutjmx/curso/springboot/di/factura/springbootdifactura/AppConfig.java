59f3c824359fa3fa27f93bfe27cd4ad5a748a0da
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2024-12-23 12:02:19 -0600

    Modificaciones 23/12/2024 12:02

--- src/main/java/com/zutjmx/curso/springboot/di/factura/springbootdifactura/AppConfig.java
+++ src/main/java/com/zutjmx/curso/springboot/di/factura/springbootdifactura/AppConfig.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
 
 import com.zutjmx.curso.springboot.di.factura.springbootdifactura.comun.DataFaker;
@@ -17,6 +18,19 @@ public class AppConfig {
 
     @Bean
     List<Item> itemsFactura() {
+        DataFaker dataFaker = new DataFaker();
+        List<Producto> productos = dataFaker.generaListaProductos();
+        List<Item> items = new ArrayList<>();
+        for (Producto producto : productos) {
+            Item item = new Item(producto, dataFaker.generaEntero());
+            items.add(item);
+        }
+        return items;
+    }
+
+    @Bean
+    @Primary
+    List<Item> itemsFacturaCasa() {
         DataFaker dataFaker = new DataFaker();
         List<Producto> productos = dataFaker.generaListaProductos();
         List<Item> items = new ArrayList<>();
